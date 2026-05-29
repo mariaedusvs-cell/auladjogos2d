b8868bf0fa9d9e1cf39a450f98a821d0a2d0c3e8
mariaedu <mariaedu.svs@gmail.com>
2026-05-29 15:44:21 -0300

    29/05 2

---
+++ Assets/Scripts/PersonagemController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonagemController : MonoBehaviour
+{
+   public Rigidbody2D rb2d;
+    void Start()
+    {
+        rb2d = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis ("Horizontal");
+        rb2d.velocity += new Vector2(1,0) * horizontalInput;
+    }
+}
