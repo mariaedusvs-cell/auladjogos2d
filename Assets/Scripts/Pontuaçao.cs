80f617aeac405d73c77d6b07adaa20c5a243e4d2
Gabriel Bezerra <mariaedu.svs@gmail.com>
2026-06-12 16:02:35 -0300

    hshg
    ssd

---
+++ Assets/Scripts/Pontuaçao.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class Pontuaçao : MonoBehaviour
+{
+    // Start is called before the first frame update
+   float points = 0;
+   public TextMeshProUGUI scoreText;
+   void Start()
+    {
+        scoreText.text = "0";
+    }
+    public void AddPoints()
+    {
+        points++;
+        scoreText.text = points.ToString();
+
+    }
+}
