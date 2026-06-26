15265c8bada0a14b4a3df66bfeaa9a3a06165a97
dutsn <mariaedu.svs@gmail.com>
2026-06-26 16:11:45 -0300

    bsbhd

--- Assets/Scripts/CoinController.cs
+++ Assets/Scripts/CoinController.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class CoinController : MonoBehaviour
 {
+    public GameObject coinParticle;
     private void OnTriggerEnter2D(Collider2D other) 
     {
         if (other.CompareTag("Player"))
         {
             Pontuaçao pontuacao = other.GetComponent<Pontuaçao>();
             pontuacao.AddPoints();
+            Instantiate(coinParticle,
+            this.transform.position,
+            Quaternion.Euler(-90,0,0));
             Destroy(this.gameObject);
         }
     }
